9985686e66830de2c092e092b41610a812f00380
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-10-17 18:44:02 +0300

    fixing square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-""" Square class """""
+""" Square module. """
 
 
-class square():
-    """ Square class """""
-
+class Square():
+    """ Square class. """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """ Constructor """
+        """ Square class initialized """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -17,18 +16,18 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        """ Permiter of the square"""
+    def permiter_of_my_square(self):
+        """ Permiter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ String representation of the square"""
+        """ String representation of the square instance """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """ Main"""
-    s = square(width=12, height=9)
+    """ Create Square instance """
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
